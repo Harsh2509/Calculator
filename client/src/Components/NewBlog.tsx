40ce794cc0a0ddcbff2549bb89978385f6c4f3da
Harsh <harshchauhan6176@gmail.com>
2024-03-03 18:06:59 +0530

    Just for testing

---
+++ client/src/Components/NewBlog.tsx
@@ -0,0 +1,90 @@
+import { Button, TextField, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "./NewBlog.css";
+import { useNavigate } from "react-router-dom";
+
+const NewBlog = (props: {
+  title: string;
+  description: string;
+  markdown: string;
+  id: number;
+  publish: boolean;
+}) => {
+  const [markdown, setMarkdown] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    setTitle(props.title);
+    setDescription(props.description);
+    setMarkdown(props.markdown);
+  }, [props.title, props.description, props.markdown]);
+
+  const navigate = useNavigate();
+
+  const buttonClickHandler = async () => {
+    const reqType = props.publish ? "POST" : "PUT";
+    const reqUrl = props.publish
+      ? `https://${import.meta.env.VITE_SECONDARY_PUBLIC_HOSTNAME}/addblog`
+      : `https://${import.meta.env.VITE_SECONDARY_PUBLIC_HOSTNAME}/blog/${
+          props.id
+        }`;
+    const response = await fetch(reqUrl, {
+      method: reqType,
+      body: JSON.stringify({
+        title,
+        description,
+        markdown,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(await response.json());
+    navigate("/");
+  };
+  return (
+    <>
+      <Typography variant="h3">New Article</Typography>
+      <Typography fontSize="1.3rem" marginTop="1%">
+        Title
+      </Typography>
+      <TextField
+        id="outlined-basic"
+        variant="outlined"
+        fullWidth
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <Typography fontSize="1.3rem" marginTop="1%">
+        Description
+      </Typography>
+      <TextField
+        id="outlined-basic"
+        variant="outlined"
+        fullWidth
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
+      <Typography fontSize="1.3rem" marginTop="1%">
+        Markdown
+      </Typography>
+      <SimpleMDE
+        value={markdown}
+        onChange={(value: string) => {
+          setMarkdown(value);
+        }}
+      />
+      <Button variant="contained" onClick={buttonClickHandler}>
+        {props.publish ? "Publish" : "Update this blog"}
+      </Button>
+    </>
+  );
+};
+
+export default NewBlog;
