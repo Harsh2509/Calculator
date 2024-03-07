0b2fba302da15d084a01e25076ef2aa936a8c692
Harsh Chauhan <91670984+Harsh2509@users.noreply.github.com>
2024-03-07 14:48:12 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,8 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./Components/Home";
-import NewBlog from "./Components/NewBlog";
-import { ElaboratedBlog } from "./Components/ElaboratedBlog";
-import { EditBlog } from "./Components/EditBlog";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route
-          path="/newBlog"
-          element={
-            <NewBlog
-              title=""
-              description=""
-              markdown=""
-              publish={true}
-              id={-1}
-            />
-          }
-        ></Route>
-        <Route path="/blog/:id" element={<ElaboratedBlog />}></Route>
-        <Route path="/editBlog/:id" element={<EditBlog />} />
-      </Routes>
-    </Router>
+    <></>
   );
 }
 
